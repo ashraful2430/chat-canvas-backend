38e97089839f0cb61f01841af4ad0cfc0ff9ba9b
ashraful2430 <www.savage582@gmail.com>
2023-11-25 15:48:14 +0600

    created a get api to load all the users

--- index.js
+++ index.js
@@ -29,6 +29,12 @@ async function run() {
     const userCollection = client.db("forumDb").collection("users");
 
     //   user related api
+
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
